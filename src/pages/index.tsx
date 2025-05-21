116caa519813d1da0470709d2b1e07dd4b76f087
Saad Najmi <sanajmi@microsoft.com>
2025-05-21 14:55:25 -0700

    fix project name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--primary button--lg"
-            to="/docs/guides/getting-started"
+            to="/docs/getting-started/create-your-first-app"
           >
             Get Started
           </Link>
