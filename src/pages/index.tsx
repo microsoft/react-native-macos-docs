d95cf98cd9cbb1df4b359760c26b015649101d9d
Saad Najmi <sanajmi@microsoft.com>
2025-05-21 22:52:03 -0700

    Copy getting started guide from windows docsite

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--primary button--lg"
-            to="/docs/getting-started/create-your-first-app"
+            to="/docs/getting-started"
           >
             Get Started
           </Link>
