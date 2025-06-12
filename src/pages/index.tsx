540790cdef54ecce5d696fa83a93156e75d200e5
Saad Najmi <sanajmi@microsoft.com>
2025-06-12 14:20:25 -0500

    Add image to header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,11 +12,18 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      <div className={styles.heroContentRow}>
+        <div className={styles.heroContentCol}>
+          <Heading as="h1" className="hero__title">
+            {siteConfig.title}
+          </Heading>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+        </div>
+        <div className={styles.heroImageCol}>
+          <img alt="React Native macOS mockup" src={"img/platform.png"} className={styles.heroImage} />
+        </div>
+      </div>
+      <div className={styles.buttonsWrapper}>
         <div className={styles.buttons}>
           <Link
             className="button button--primary button--lg"
