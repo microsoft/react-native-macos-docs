147a835bb163be8e960fa47812a7e92b997b4c6a
Saad Najmi <sanajmi@microsoft.com>
2025-06-12 17:47:26 -0500

    Update homepage features

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -2,6 +2,7 @@ import type {ReactNode} from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import { DesktopMacRegular } from '@fluentui/react-icons';
 
 type FeatureItem = {
   title: string;
@@ -12,23 +13,23 @@ type FeatureItem = {
 const FeatureList: FeatureItem[] = [
   {
     title: 'Truly Native',
-    Svg: require('@site/static/img/react-logo.svg').default,
+    Svg: require('@site/static/img/ic_fluent_desktop_mac_32_regular.svg').default,
     description: (
       <>Built on top of Appkit, Apple's macOS Platform SDK</>
     ),
   },
   {
-    title: 'Cross Platform',
-    Svg: require('@site/static/img/react-logo.svg').default,
+    title: 'Part of the Ecosystem',
+    Svg: require('@site/static/img/ic_fluent_plug_connected_48_regular.svg').default,
     description: (
       <>Use your favorite React Native libraries</>
     ),
   },
   {
     title: 'Supported by Microsoft',
-    Svg: require('@site/static/img/react-logo.svg').default,
+    Svg: require('@site/static/img/ic_fluent_app_generic_48_filled.svg').default,
     description: (
-      <>Used at scale by some of the largest apps out there</>
+      <>Used at scale in production by some of the largest apps out there</>
     ),
   },
 ];
