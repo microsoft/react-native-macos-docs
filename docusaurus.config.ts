b937877cc018f6baa65e290088bbee749dbec453
Saad Najmi <sanajmi@microsoft.com>
2025-06-16 19:55:36 -0500

    chore: create initial docs (#8)
    * chore: create scaffold project using `create-docusaurus`
    
    * move dependabot config file to inside .github
    
    * Add build PR workflow
    
    * Remove blog
    
    * Replace docusaurus template with scaffold docs
    
    * fix project name
    
    * Add github link
    
    * Add Microsoft Open Source footer
    
    * Remove links from footer
    
    * Use npm to run lint:commit
    
    * Copy getting started guide from windows docsite
    
    * draft an intro section
    
    * Draft a page about native development
    
    * Add blog link
    
    * Add platform.png
    
    * Add image to header
    
    * Remove the background, update theme
    
    * Update features
    
    * Update homepage features
    
    ---------
    
    Co-authored-by: Julio C. Rocha <julio.rocha@microsoft.com>

---
+++ docusaurus.config.ts
@@ -0,0 +1,92 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const organizationName = "microsoft";
+const projectName = "react-native-macos";
+const projectFriendlyName = "React Native macOS";
+const githubUrl = "https://github.com/" + organizationName + "/" + projectName;
+const mainBranchUrl = githubUrl + "/tree/main";
+const docsiteUrl = mainBranchUrl + "/docsite";
+
+const config: Config = {
+  title: 'React Native macOS',
+  tagline: 'Build native macOS apps using React Native',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://' + organizationName + '.github.io',
+  baseUrl: "/" + projectName + "/",
+
+  organizationName,  
+  projectName,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          editUrl: docsiteUrl + "/",
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: 'img/react-logo.svg',
+    navbar: {
+      title: projectFriendlyName,
+      logo: {
+        alt: 'react native logo',
+        src: 'img/react-logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'docsSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          label: "Blog",
+          position: "right",
+          href: "https://devblogs.microsoft.com/react-native/",
+        },
+        {
+          src: "img/github-logo.svg",
+          href: githubUrl,
+          className: "github-mark-24px",
+          position: "right",
+          "aria-label": "GitHub repository",
+        },
+      ],
+    },
+    footer: {
+      logo: {
+        src: "img/msoss-light.png",
+        srcDark: "img/msoss-dark.png",
+        alt: "Microsoft Open Source logo",
+        href: "https://opensource.microsoft.com",
+      },
+      copyright: `Copyright © ${new Date().getFullYear()} Microsoft`,
+    },
+    prism: {
+      theme: prismThemes.vsLight,
+      darkTheme: prismThemes.vsDark,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,64 @@
+import type {ReactNode} from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Heading from '@theme/Heading';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero', styles.heroBanner)}>
+      <div className={styles.heroContentRow}>
+        <div className={styles.heroContentCol}>
+          <Heading as="h1" className="hero__title">
+            {siteConfig.title}
+          </Heading>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+        </div>
+        <div className={styles.heroImageCol}>
+          <img alt="React Native macOS mockup" src={"img/platform.png"} className={styles.heroImage} />
+        </div>
+      </div>
+      <div className={styles.buttonsWrapper}>
+        <div className={styles.buttons}>
+          <Link
+            className={clsx(
+              "button button--primary button--lg",
+              styles.headerButton
+            )}
+            to="/docs/getting-started"
+          >
+            Get Started
+          </Link>
+          <Link
+            className={clsx(
+              "button button--link button--lg",
+              styles.headerLink
+            )}
+            to="/docs/intro"
+          >
+            Learn the Basics
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): ReactNode {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
